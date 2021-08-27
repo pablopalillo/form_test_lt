d4f88970eaf5768ecb970f797f800ab4c59f15c9
pablopalillo <pablo.576@hotmail.com>
2021-08-27 03:10:23 -0500

    reformat code

--- companies/views.py
+++ companies/views.py
@@ -1,10 +1,10 @@
 from django.http import Http404, HttpResponseBadRequest
-from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from django.shortcuts import get_object_or_404
-from rest_framework.authentication import BasicAuthentication
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.authentication import BasicAuthentication
 from .models import Companies
 from .serializers import CompaniesSerializer
 
@@ -36,8 +36,15 @@ class CompanyView(APIView):
 
     def put(self, request):
         try:
-            company_data = get_object_or_404(Companies, id_company=request.data.get('id_company'))
-            serializer_company = CompaniesSerializer(company_data, data=request.data)
+            company_data = get_object_or_404(
+                Companies,
+                id_company=request.data.get('id_company')
+            )
+
+            serializer_company = CompaniesSerializer(
+                company_data,
+                data=request.data
+            )
 
             if serializer_company.is_valid(raise_exception=True):
                 serializer_company.save()
