2cda792be459d2617074aa5d9cf035559786b1e8
pablopalillo <pablo.576@hotmail.com>
2021-08-27 02:01:56 -0500

    auth permisions

--- companies/serializers.py
+++ companies/serializers.py
@@ -6,4 +6,25 @@ class CompaniesSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Companies
-        exclude = ['id_company']
+        fields = '__all__'
+
+    def validate_company_telephone(self, value):
+        """
+        Check if telephone is number
+        """
+        tel_fix_blank_spaces = value.replace(" ", "")
+        if not tel_fix_blank_spaces.isnumeric():
+            raise serializers.ValidationError("Telephone is not valid.")
+        return value
+
+    def validate_company_nit(self, nit):
+        """
+        Check if nit is valid
+        :param nit: numeric value where accept only - character for consecutive value
+        :return: nit
+        """
+        nit_fix_accept_value = nit.replace("-", "")
+        nit_fix_blank_spaces = nit_fix_accept_value.replace(" ", "")
+        if not nit_fix_blank_spaces.isnumeric():
+            raise serializers.ValidationError("Nit is not valid.")
+        return nit
--- companies/views.py
+++ companies/views.py
@@ -1,5 +1,6 @@
-from django.http import Http404
-from django.core.exceptions import ObjectDoesNotExist
+from django.http import Http404, HttpResponseBadRequest
+from django.core.exceptions import ObjectDoesNotExist, ValidationError
+from rest_framework.authentication import BasicAuthentication
 from rest_framework import permissions
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -9,7 +10,8 @@ from .serializers import CompaniesSerializer
 
 class CompanyView(APIView):
 
-    permission_classes = [permissions.AllowAny]
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def get(self, request):
         try:
@@ -20,3 +22,13 @@ class CompanyView(APIView):
             raise Http404
 
         return Response(serializer_company.data)
+
+    def post(self, request):
+        try:
+            serializer_company = CompaniesSerializer(data=request.data)
+            if serializer_company.is_valid(raise_exception=True):
+                serializer_company.save()
+            return Response(serializer_company.data)
+
+        except ValidationError:
+            raise HttpResponseBadRequest
